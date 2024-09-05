44653eac8f8ebc93e2c6d2797a2274ecd5f0806a
elcharitas <jonathanirhodia@gmail.com>
2024-09-06 00:49:19 +0100

    chore: use handler in ngyn example

--- benchmark/hello-world/ngyn/src/main.rs
+++ benchmark/hello-world/ngyn/src/main.rs
@@ -1,13 +1,10 @@
 use ngyn::prelude::*;
-use ngyn_hyper::HyperApplication;
 
 #[tokio::main]
 async fn main() {
     let mut app = HyperApplication::default();
 
-    app.get("*", |_cx: &mut NgynContext, res: &mut NgynResponse| {
-        *res.body_mut() = "Hello, World!".into();
-    });
+    app.any("*", handler(|_cx: &mut NgynContext| "Hello, World!"));
 
     let _ = app.listen("0.0.0.0:3000").await;
 }
